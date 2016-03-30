6153129c8fa6fc7734ba8c3251e03e57393133a8
Giovani Sousa <sousa_giovani@hotmail.com>
2016-03-30 02:11:02 -0300

    Update Utils.java
    Conversão de binario para decimal conforme binario-real+page+1.pdf e binario-real+page+2.pdf postados pela Sara.

--- Codigo/algoritmosGeneticos/Utils.java
+++ Codigo/algoritmosGeneticos/Utils.java
@@ -1,14 +1,25 @@
 package algoritmosGeneticos;
 
-//A classe Utils implementará funções diversas (principalmente de manipulação de binários)
-//que não fazem parte do escopo do problema mas são necessárias para tal
+//A classe Utils implementarÃ¡ funÃ§Ãµes diversas (principalmente de manipulaÃ§Ã£o de binÃ¡rios)
+//que nÃ£o fazem parte do escopo do problema mas sÃ£o necessÃ¡rias para tal
 public class Utils {
 
 	//binarioPraDecimal -> Recebe como entrada um array de int que representa um ponto no plano e retorna o ponto
-	//Ainda precisamos definir tamanho de geração, mantissa, expoente, e etc para modelar essa função
-	//Essa é a função de mapeamento que mapeiao genótipo em fenótipo
-	Ponto binarioPraDecimal(int[] entrada)
+	//Ainda precisamos definir tamanho de geraÃ§Ã£o, mantissa, expoente, e etc para modelar essa funÃ§Ã£o
+	//Essa Ã© a funÃ§Ã£o de mapeamento que mapeiao genÃ³tipo em fenÃ³tipo
+	//entrada Ã© o genoma, min e max sÃ£o as definiÃ§Ãµes do intervalo (dependentes da funcao).
+	static Ponto binarioPraDecimal(int[] entrada, int min, int max)
 	{
-		return new Ponto(0, 0);
+            double x = 0, y = 0;
+            for(int i =0; i < entrada.length/2; i++)
+            {   
+                x+= Math.pow(2, i)*(entrada[entrada.length/2-1-i]);
+                y+= Math.pow(2, i)*(entrada[entrada.length-1-i]);
+            }
+            
+            x = min + x*((max-min)/(Math.pow(2, entrada.length/2)-1));
+            y = min + y*((max-min)/(Math.pow(2, entrada.length/2)-1));
+            
+            return new Ponto(x, y);
 	}
 }
