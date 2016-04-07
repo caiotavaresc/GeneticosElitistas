5162c3b8e8661d5be9eb52c1816bfeb2ca8da4d1
Giovani Sousa <sousa_giovani@hotmail.com>
2016-04-07 10:58:54 -0300

    Individuo para guardar genótipo+fitness

---
+++ Codigo/algoritmosGeneticos/Individuo.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package algoritmosGeneticos;
+
+import java.util.Arrays;
+
+/**
+ *
+ * @author sousa
+ */
+public class Individuo implements Comparable<Individuo>{
+    private int [] genotipo;
+    protected double fitness;
+    
+    public Individuo(int[] f, double fit)
+    {
+        genotipo = Arrays.copyOf(f, f.length);
+        fitness = fit;
+    }
+    
+    int [] getGenotipo()
+    {
+        return genotipo;
+    }
+
+    @Override
+    public int compareTo(Individuo o) {
+        return (int)(this.fitness - o.fitness);
+    }
+}
